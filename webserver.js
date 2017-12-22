5255b72304471de0e1de6ad57bcf1d003badeaec
manpreet <sidhumanpreet391@gmail.com>
2017-12-22 14:42:46 -0500

    newname

--- webserver.js
+++ webserver.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-<<<<<<< HEAD
+//<<<<<<< HEAD
 var sFolder=".";
 var app = connect();
 app.use(serveStatic("."));
@@ -9,15 +9,15 @@ app.listen(nport,()=>{
     console.log("Listening on Port: "+nport);
 console.log("Document root:"+__dirname+"/"+sFolder);
 });
-=======
+//=======
 
-var app = connect();
-var sFolder = ".";
-app.use(serveStatic(sFolder));
-var nPort = 5100;
-app.listen(nPort, ()=>{
-    console.log("Listening on port: " + nPort);
-    console.log("Document Root: " + __dirname + "/" + sFolder);
-});
+// var app = connect();
+// var sFolder = ".";
+// app.use(serveStatic(sFolder));
+// var nPort = 5100;
+// app.listen(nPort, ()=>{
+//     console.log("Listening on port: " + nPort);
+//     console.log("Document Root: " + __dirname + "/" + sFolder);
+// });
 
->>>>>>> 6366a1c237fe46e4e7c8ba1a7caa7557eeba587f
+//>>>>>>> 6366a1c237fe46e4e7c8ba1a7caa7557eeba587f
